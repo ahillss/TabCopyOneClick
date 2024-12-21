cb6e36c36f5b8257e1091ca83c152c4851078a9e
ahillss <ahillss@users.noreply.github.com>
2024-12-21 23:51:54 +1100

    aa

--- background.js
+++ background.js
@@ -1,41 +1,39 @@
-
-var eol = (navigator.platform.indexOf("Win") !== -1)?'\r\n' : '\n';
-
 async function addToClipboard(value) {
-    await chrome.offscreen.createDocument({url:'offscreen.html',reasons:[chrome.offscreen.Reason.CLIPBOARD],justification:'Write text to the clipboard.'});
-    chrome.runtime.sendMessage({ type: 'copy-data-to-clipboard', target: 'offscreen-doc', data: value });
+    await chrome.offscreen.createDocument({
+        url:'offscreen.html',
+        reasons:[chrome.offscreen.Reason.CLIPBOARD],
+        justification:'Write text to the clipboard.'
+    });
+    
+    chrome.runtime.sendMessage({ 
+        type: 'copy-data-to-clipboard', 
+        target: 'offscreen-doc', 
+        data: value 
+    });
 }
-
-//async function addToClipboardV2(value) {  navigator.clipboard.writeText(value); }
 
 function get_tabs(ts) {
     try {
+        var eol = (navigator.platform.indexOf('Win') !== -1)?'\r\n' : '\n';
         var str = '';
-
-        for (var i = 0; i < ts.length; i++) {
-            if (str.length) {
-                str += eol;
-            }
-
-            str += ts[i].url;
-        }
+        for(var i=0;i<ts.length;i++) { str += ts[i].url + eol; }
         
+        //navigator.clipboard.writeText(str);
         addToClipboard(str);
-                
-        chrome.action.setIcon({ path: {"64": "/icon/icon64b.png"} });
         
-        setTimeout(function(){
-            chrome.action.setIcon({ path: {"64": "/icon/icon64a.png"} });
-        }, 150);
+        chrome.action.setIcon({path: {'64': '/icon/icon64b.png'}});        
+        setTimeout(function(){ chrome.action.setIcon({path: {'64': '/icon/icon64a.png'}}); }, 125);
     } catch (ex) {
         console.error(ex);
     }
 }
 
 chrome.action.onClicked.addListener(function(tab) {
-    try {
-        chrome.tabs.query({windowId:tab.windowId,highlighted:true}, get_tabs);
-    } catch (ex) {
-        console.error(ex);
-    }
+    chrome.tabs.query({windowId:tab.windowId,highlighted:true}, function(ts) {
+        if(ts.length==1) {
+            chrome.tabs.query({windowId:tab.windowId}, get_tabs);
+        } else {
+            get_tabs(ts);
+        }            
+    });
 });
